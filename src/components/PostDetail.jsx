7d730a104b2a7fe1d60a3c7ceadfd547e7e524f5
oh <outputor777@gmail.com>
2022-08-25 01:27:33 +0900

    게시글 수정 오류, 댓글 추가 오류

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -5,10 +5,12 @@ import styled from "styled-components"
 import design1 from "../assets/design1.jpg"
 import { __getComments } from "../redux/modules/InstaSlice";
 
-const PostDetail = ({ setModalOpen, boardId }) => {
+const PostDetail = ({ setModalOpen, boardId, }) => {
 
 
     console.log('boardId', boardId)
+
+
     const dispatch = useDispatch();
     const data = useSelector((state) => state.instas.insta.data)
 
@@ -36,6 +38,7 @@ const PostDetail = ({ setModalOpen, boardId }) => {
                     commentList={data.commentList}
                     boardContent={data.content}
                     username={data.username}
+                    boardId={boardId}
                 />
             </PostDetailContainer>
         </Background>
