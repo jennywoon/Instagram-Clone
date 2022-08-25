a3d717bcd45937b9ef79b30901b46f044004f653
jennywoon <cjaeks0394@naver.com>
2022-08-25 19:27:14 +0900

    수정

--- src/components/MainPost.jsx
+++ src/components/MainPost.jsx
@@ -150,8 +150,8 @@ const MainPost = ({
         const confirm = window.confirm('정말 삭제하시겠습니까?')
         if (confirm) {
             dispatch(__deleteInsta(Number(cookies.get('boardId'))));
+            window.location.reload();
         }
-        window.location.reload();
     }
 
     const onClickPutModalHandler = () => {
